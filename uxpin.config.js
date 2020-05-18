30f95cf1cdf2c14870a25e5beed2ba3373c489fb
Radosław Taraszka <radoslawtaraszka@Radosaws-MBP.aircard>
2020-05-18 15:06:41 +0200

    camera disabled

--- uxpin.config.js
+++ uxpin.config.js
@@ -6,7 +6,7 @@ module.exports = {
         name: 'General',
         include: [
           'src/components/Button/Button.js',
-          'src/components/Camera/Camera.js',
+          // 'src/components/Camera/Camera.js',
           'src/components/CreditCard/CreditCard.jsx',
           'src/components/GoogleMap/GoogleMap.js',
           'src/components/Greeting/Greeting.js',
@@ -27,6 +27,8 @@ module.exports = {
         ],
       },
     ],
+    wrapper: 'src/components/UXPinWrapper/UXPinWrapper.js',
+    webpackConfig: 'webpack.uxpin.config.js',
   },
   name: 'UXPin Merge Boilerplate',
 };
